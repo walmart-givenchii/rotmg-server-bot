4a4f4c6a46856e1ba9ec1c743291c399ea2310ae
walmart-givenchii <joesgot2bcool@gmail.com>
2017-05-20 18:12:29 -0700

    this is a mess

--- brain.rb
+++
@@ -1,9 +0,0 @@
-#this is going to be mainly in ruby now
-require = 'discordrb'
-
-bot.run :async
-
-bot = Discordrb::Bot.new token: 'MzE1NjM3NjIyNzQxNjYzNzQ0.DAJoKg.v-0FQ69RBU8Lt2bf_yS2ryQlWhA', client_id: 315637622741663744
-bot.message(with_text: "Hey Bot!") do |event|
-  event.respond "Hi, #{event.user.name}!"
-end
